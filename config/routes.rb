42985434e938e00c654e7d406a3940de6aa1cff2
Luis Fernando <lfmnovaes@gmail.com>
2022-01-12 19:22:28 -0300

    Fix routing again and delete some unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root :to => redirect('/users')
-  resource :users, only: [:index, :show] do
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 end
