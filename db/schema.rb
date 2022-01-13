b39aa2428452ae481bbdb7a6c23faf5b9da6b758
Luis Fernando <lfmnovaes@gmail.com>
2022-01-13 20:15:58 -0300

    Add required counters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_223442) do
+ActiveRecord::Schema.define(version: 2022_01_13_230604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,15 +40,18 @@ ActiveRecord::Schema.define(version: 2022_01_13_223442) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
+    t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
