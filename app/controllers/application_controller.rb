e60be94bb6867b79312a7f9a26cf2685f129c0d3
Luis Fernando <lfmnovaes@gmail.com>
2022-01-17 18:15:43 -0300

    Add methods to models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def current_user
     User.first
   end
