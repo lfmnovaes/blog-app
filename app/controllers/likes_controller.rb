2ee22899276c48d81890dac1b039f79eebd4be81
Luis Fernando <lfmnovaes@gmail.com>
2022-01-20 19:28:54 -0300

    Fix typo

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(user: current_user)
-    @like.update_counter if @like.savenp
+    @like.update_counter if @like.save
     redirect_to [@post.user, @post]
   end
 end
