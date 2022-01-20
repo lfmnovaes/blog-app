ec0c3ff8221b77f9efcb5323fb0ec36d5433dc53
Luis Fernando <lfmnovaes@gmail.com>
2022-01-20 19:28:28 -0300

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(user: current_user)
-    if @like.save
-      @like.update_counter
-      redirect_to [@post.user, @post]
-    else
-      redirect_to [@post.user, @post]
-    end
+    @like.update_counter if @like.savenp
+    redirect_to [@post.user, @post]
   end
 end
