b13cbe43caf1a0538423ec1af2b00dd996308ef8
Luis Fernando <lfmnovaes@gmail.com>
2022-01-21 18:11:42 -0300

    Quick fix to prevent another n+1 querie problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @post = @user.posts.includes(:comments).find(params[:id])
     @comments = @post.comments.all.order(created_at: :desc)
   end
 
