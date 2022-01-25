9f1fb37b186e836a07c2554252efffd4a8a6d4f8
mike2611 <mpuentes2611@gmail.com>
2022-01-24 18:19:00 -0600

    Add configurations for devise and modify schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'users#index'
+
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end 
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create]
