5a8764442e93a3370b42cc69b0b4cd629c4d5d2b
Luis Fernando <lfmnovaes@gmail.com>
2022-01-25 15:03:18 -0300

    Fix routing after sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root 'users#index'
-
+  devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy' 
-  end 
-
+  end
+  root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create]
     resources :posts, only: [:show] do
