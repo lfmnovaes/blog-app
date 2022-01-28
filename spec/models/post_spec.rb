954582aecaba78de0ae1e3db677a7edcb5ee3f0e
Luis Fernando <lfmnovaes@gmail.com>
2022-01-28 00:43:14 -0300

    Fix models spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before(:each) do
-    @user = User.new(name: 'John', posts_counter: 1)
-    @post = @user.posts.new(title: 'Lorem Ipsum', comments_counter: 0, likes_counter: 0)
+    @user = User.new(name: 'John', photo: 'https://i.pravatar.cc/200?img=3', bio: 'john bio', posts_counter: 1,
+    email: 'john@mail.com', password: 'pass123')
+    @user.skip_confirmation!
+    @post = @user.posts.new(title: 'Lorem Ipsum', text: 'some text', comments_counter: 0, likes_counter: 0)
     @user.save
   end
   describe 'check if title' do
